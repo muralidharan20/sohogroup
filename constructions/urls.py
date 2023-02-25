e28b39bc1f3713977d22f5a46e54564ab073346c
muralidharan20 <70807333+muralidharan20@users.noreply.github.com>
2023-02-25 10:24:29 +0530

    Update urls.py

--- constructions/urls.py
+++ constructions/urls.py
@@ -17,7 +17,5 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('core.urls')),
-    path('api/',include('api.urls'))
+    path('admin/', admin.site.urls)
 ]
